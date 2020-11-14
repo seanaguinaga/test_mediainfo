2bcd5cd2249c4cb34720625932d363b793431ac7
ottonove <niigata01web@gmail.com>
2020-11-14 18:54:05 +0900

    develop環境とproduction環境の両方で動作する様になった。 "MediaInfoModule.wasm"は、buildの1つ上の階層にコピーされる。

--- config-overrides.js
+++ config-overrides.js
@@ -2,13 +2,18 @@ const path = require("path");
 const { resolve } = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
+const devMode = process.env.NODE_ENV !== 'production'
+
 const wasmFile = resolve(
   __dirname,
   'node_modules',
   'mediainfo.js',
-  'dist'
+  'dist',
+  'MediaInfoModule.wasm'
 );
-const dist = resolve('.', 'build'/* , 'static', 'js' */);
+
+const dist = devMode ? '.' : resolve(__dirname);
+// const dist = resolve(__dirname);
 
 module.exports = function override(config, env) {
   config.module.rules.push({
@@ -39,7 +44,7 @@ module.exports = function override(config, env) {
   // template from https://www.npmjs.com/package/react-app-rewired
   config.plugins.push(new CopyPlugin({
     patterns: [
-      { from: wasmFile, to: __dirname },
+      { from: wasmFile, to: dist },
     ],
   }),)
       
