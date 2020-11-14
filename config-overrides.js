7419485fd5bc9c75702c7f45252befdefbc5a569
ottonove <niigata01web@gmail.com>
2020-11-14 11:30:09 +0900

    Add the following npm package. "mediainfo.js": "^0.1.3", "copy-webpack-plugin": "^6.3.1", "react-app-rewired": "^2.1.6"

---
+++ config-overrides.js
@@ -0,0 +1,22 @@
+const { resolve } = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const wasmFile = resolve(
+  __dirname,
+  'node_modules',
+  'mediainfo.js',
+  'dist'
+);
+const dist = resolve('build', 'static', 'js');
+
+// template from https://www.npmjs.com/package/react-app-rewired
+module.exports = {
+  webpack: function(config, env) {
+    config.plugins.push(new CopyPlugin({
+      patterns: [
+        { from: wasmFile, to: dist },
+      ],
+    }),)
+    return config;
+  },
+}
--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import MediaInfo from 'mediainfo.js';
+
 function App() {
   return (
     <div className="App">
